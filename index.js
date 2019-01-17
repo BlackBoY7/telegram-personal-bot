15384b4a3fbb1b1d0e57c05376d268435c1eaa6c
eegad <sabu.siyad.p@gmail.com>
2019-01-17 12:09:03 +0000

    reports to owner

--- index.js
+++ index.js
@@ -3,7 +3,13 @@
 var config = require('./config');
 
 //start bot
-var telegram = require('telegram-bot-api');
-var bot = new telegram({
-    token: config.token
+var telegram = require('node-telegram-bot-api');
+var bot = new telegram(config.token, {polling: true});
+
+bot.on('message', (msg) => {
+    //check if message is from owner else report
+    if(msg.from.id != config.owner.id){
+        bot.sendMessage(config.owner.id, 'New message from ${msg.from.id}: ${msg.text}');
+        return;
+    };
 });
