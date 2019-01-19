1a90843a9853ce64c55524242dc436f46a43b38b
eegad <sabu.siyad.p@gmail.com>
2019-01-19 22:11:27 +0530

    added

---
+++ models/notes.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../utils/database');
+
+//create notes model
+var notes = sequelize.define('notes', {
+	id: {
+		type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+	},
+    title: {
+        type: Sequelize.STRING,
+	    allowNull: false
+    },
+    content: {
+        type: Sequelize.STRING,
+	    allowNull: false
+    }
+});
+
+module.exports = notes;
