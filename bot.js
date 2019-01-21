cc2c2ab94930bad67c267ba6ba8bcb566890155e
eegad <sabu.siyad.p@gmail.com>
2019-01-21 23:29:42 +0530

    stage

--- bot.js
+++ bot.js
@@ -1,29 +1,65 @@
 //import config
-const config = require('./config');
+const config = require("./config");
 
 //start bot
-const telegram = require('node-telegram-bot-api');
-const bot = new telegram(config.token, {polling: true});
-const sequelize = require('./utils/database');
+const Telegraf = require("telegraf");
+const stage = require("telegraf/stage")
+const bot = new Telegraf(config.token);
+const sequelize = require("./utils/database");
+
+//start bot
+bot.startPolling();
+bot.use(session())
+bot.use(stage.middleware())
+
+//commands
+var cmd1 = "#addnote";
 
 //import db models
-const notes = require('./models/notes');
+const notes = require("./models/notes");
 
+//add note scene
+const addNote = new WizardScene('add-note',
+    (ctx) => {
+	    ctx.reply("let's add a note!\nTitle of note:");
+		return ctx.wizard.next()
+    },
+    (ctx) => {
+	    Title = ctx.message.text;
+		ctx.reply('Content of Note: ')
+		return ctx.wizard.next()
+    },
+    (ctx) => {
+		Content = ctx.message.text;
+		notes.create({
+			title: Title,
+			content: Content
+        }).then(function(err) {
+			if(err){
+				ctx.reply(err);
+			}else{
+				ctx.reply("Done!");
+			}
+		});
+		return ctx.scene.leave()
+    }
+);
 
-bot.on('message', (msg) => {
-    //check if message is from owner else report
-    if(msg.from.id != config.owner.id){
-        bot.sendMessage(config.owner.id, 'New message from ${msg.from.id}: ${msg.text}');
-        return;
-    };
-});
+//core
+bot.on('text', (ctx) => {
+	if(ctx.from.id != config.owner.id){
+		return;
+	}else{
+		bot.hears(cmd1, stage.enter('add-note'));
+	}
+}
 
-//To test database connection
+//to test database connection
 sequelize.authenticate().then(function (err) {
     if(err){
-        bot.sendMessage(config.log, 'Unable to connect to the database: ' + err);
+        bot.sendMessage(config.owner.id, `Unable to connect to the database: ${err}`);
     }else{
         sequelize.sync();
-        bot.sendMessage(config.log, 'Database connection has been established successfully.');
+        bot.sendMessage(config.owner.id, "Connected to database");
     }
 });
