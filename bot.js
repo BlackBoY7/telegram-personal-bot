00926495c94d7d286af46059b1e8f2b38b815d83
eegad <sabu.siyad.p@gmail.com>
2019-01-23 12:16:28 +0530

    arrangements

--- bot.js
+++ bot.js
@@ -3,63 +3,64 @@ const config = require("./config");
 
 //start bot
 const Telegraf = require("telegraf");
-const stage = require("telegraf/stage");
+const Stage = require("telegraf/stage");
 const session = require("telegraf/session");
-const wizardScene = require("telegraf/scenes/base");
+const wizardScene = require("telegraf/scenes/wizard");
 const bot = new Telegraf(config.token);
 const sequelize = require("./utils/database");
 
-//start bot
-bot.startPolling();
-bot.use(session());
-bot.use(stage.middleware());
-
 //commands
-var cmd1 = "#addnote";
+var cmd1 = "addnote";
 
 //import db models
 const notes = require("./models/notes");
 
 //add note scene
-const addNote = new wizardScene('add-note',
+const addNote = new wizardScene("addnote",
     (ctx) => {
-	    ctx.reply("let's add a note!\nTitle of note:");
-		return ctx.wizard.next();
+	      ctx.reply("let's add a note!\nTitle of note:");
+		    return ctx.wizard.next();
     },
     (ctx) => {
-	    Title = ctx.message.text;
-		ctx.reply('Content of Note: ');
-		return ctx.wizard.next();
+	      Title = ctx.message.text;
+		    ctx.reply('Content of Note: ');
+		    return ctx.wizard.next();
     },
     (ctx) => {
-		Content = ctx.message.text;
-		notes.create({
-			title: Title,
-			content: Content
+		    Content = ctx.message.text;
+		    notes.create({
+			      title: Title,
+			      content: Content
         }).then(function(err) {
-			if(err){
-				ctx.reply(err);
-			}else{
-				ctx.reply("Done!");
-			}
-		});
-		return ctx.scene.leave()
+			      if(err){
+				        ctx.reply(err);
+			      }else{
+				        ctx.reply("Done!");
+			      }
+		   });
+		   return ctx.wizard.next();
     }
 );
 
+//start bot
+const stage = new Stage([addNote]);
+bot.use(session());
+bot.use(stage.middleware());
+bot.startPolling();
+
 //core
+bot.start((ctx) => ctx.reply(`Hello ${ctx.from.first_name}!`));
 bot.on('text', (ctx) => {
-	if(ctx.from.id != config.owner.id){
-		return;
-	}else{
-		bot.hears(cmd1, stage.enter('add-note'));
-	}
+    if(ctx.from.id != config.owner.id){
+        return;
+    }
 });
+bot.hears(cmd1, Stage.enter("addnote"));
 
 //to test database connection
 sequelize.authenticate().then(function (err) {
     if(err){
-        bot.sendMessage(config.owner.id, `Unable to connect to the database: ${err}`);
+        bot.telegram.sendMessage(config.owner.id, `Unable to connect to the database: ${err}`);
     }else{
         sequelize.sync();
         bot.telegram.sendMessage(config.owner.id, "Connected to database");
